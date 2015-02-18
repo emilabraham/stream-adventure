5593b799c96a4a349babdb18a1ae68fd6502c380
Emil Abraham <emilabraham1414@gmail.com>
2015-02-17 19:40:59 -0500

    Doh. zlib is already included in node core.

--- combiner.js
+++ combiner.js
@@ -1,7 +1,7 @@
 var combine = require('stream-combiner');
 var split = require('split');
 var through = require('through');
-var zlib - require('zlib');
+var zlib = require('zlib');
 
 module.exports = function () {
   return combine(process.stdin, split(JSON.parse), process.stdout);
