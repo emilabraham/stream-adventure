0b02ac38321f72febd513d84dc8473725116745a
Emil Abraham <emilabraham1414@gmail.com>
2015-02-18 10:36:41 -0500

    Finished the cryptography exercise. Only 1 exercise left.

--- combiner.js
+++ combiner.js
@@ -3,6 +3,35 @@ var split = require('split');
 var through = require('through');
 var zlib = require('zlib');
 
+// Used a solution I found online as a guide
 module.exports = function () {
-  return combine(process.stdin, split(JSON.parse), process.stdout);
+  var category;
+
+  function write (data) {  
+    if(data.length === 0) {
+      return;
+    }
+    var parsed = JSON.parse(data);
+
+    if (parsed.type === 'genre') {
+      if (category){
+        this.queue(JSON.stringify(category) + '\n');
+      }
+      category = { name: parsed.name, books: [] };
+    }
+    else if (parsed.type === 'book'){
+      category.books.push(parsed.name);
+    }
+  }
+
+  function end () {
+    if (category) {
+      this.queue(JSON.stringify(category) + '\n');
+    }
+    this.queue(null);
+  }
+
+  var organize = through(write, end);
+
+  return combine(split(), organize, zlib.createGzip());
 }
